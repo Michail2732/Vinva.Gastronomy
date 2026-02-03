0167059cb99859fddd7ef627f2e7a50104ca4b71
Michail2732 <mihailerichev@gmail.com>
2026-02-03 23:57:43 +0300

    Написал тесты для хендлеров запросов

---
+++ src/Recipes/Vinva.Gastronomy.Recipes.Tests/Application/GetRecipeByIngredientsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinva.Gastronomy.Recipes.Tests.Application
+{
+    internal class GetRecipeByIngredientsTests
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ src/Recipes/Vinva.Gastronomy.Recipes.Tests/Application/GetRecipesByCategoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinva.Gastronomy.Recipes.Tests.Application
+{
+    internal class GetRecipesByCategoryTests
+    {
+
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
